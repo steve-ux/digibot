e77ea034dabaff9d6f0c8e22bf58d1a28489db4c
steve-ux <exe.universal@gmail.com>
2026-07-13 23:02:01 -0300

    Se cambia el tag de google ads para empezar campañas publicitarias con metricas

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,21 +28,27 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="es" className={`${inter.variable} ${interTight.variable} ${jetBrainsMono.variable} ${dsdigital.variable} dark`}>
       <head>
         <Script crossOrigin="anonymous" src="//unpkg.com/same-runtime/dist/index.global.js" />
-        {/* Google tag (gtag.js) */}
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-GXPMFGD4NM"
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
           {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-GXPMFGD4NM');
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NMD4HQSZ');
           `}
         </Script>
       </head>
       <body suppressHydrationWarning className="antialiased">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NMD4HQSZ"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         {children}
         <CookieBanner />
       </body>
